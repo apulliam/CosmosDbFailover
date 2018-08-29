8743ced4230f16e7c04742fd4c05443d4b6c49f3
Alan Pulliam <apulliam@microsoft.com>
2018-08-28 21:54:47 -0400

    updated .net version

---
+++ Untitled1.ps1
@@ -0,0 +1,11 @@
+﻿
+$pwd = Read-Host -Prompt "Enter password" -AsSecureString
+
+# Create the service principal (use a strong password)
+$sp = New-AzureRmADServicePrincipal -DisplayName "apulliam-cosmosdb-manager" -Password $pwd
+
+# Give it the permissions it needs...
+New-AzureRmRoleAssignment -ServicePrincipalName $sp.ApplicationId -RoleDefinitionName Contributor
+
+# Display the Application ID, because we'll need it later.
+$sp | Select DisplayName, ApplicationId
